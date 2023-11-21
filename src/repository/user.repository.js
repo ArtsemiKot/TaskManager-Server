20b4d28b859198bf449cb69cff78d1dc283096a0
ArtsemiKot <kot_artemiy@mail.ru>
2023-11-21 20:18:22 +0300

    taskmanager-server

--- src/repository/user.repository.js
+++ src/repository/user.repository.js
@@ -16,38 +16,69 @@ async function getUserIDDB(id) {
 
 async function createUsersDB(name, surname, email, pwd) {
   const client = await pool.connect();
-  const sql = `insert into users(name, surname, email, pwd) values
-    ($1,$2,$3,$4) returning *`;
-  const data = (await client.query(sql, [name, surname, email, pwd])).rows;
-  return data;
+  try {
+    await client.query('BEGIN');
+    const sql = `insert into users(name, surname, email, pwd) values
+      ($1,$2,$3,$4) returning *`;
+    const data = (await client.query(sql, [name, surname, email, pwd])).rows;
+    await client.query('COMMIT');
+    return data;
+  } catch (error) {
+    await client.query('ROLLBACK');
+    console.log(`createUserDB: ${error.message}`);
+    return [];
+  }
 }
 async function updateUsersDB(id, name, surname, email, pwd) {
   const client = await pool.connect();
-  const sql = `update users set 
-    name =$1, surname=$2, email = $3, pwd = $4
-    where id = $5 returning *`;
-  const data = (await client.query(sql, [name, surname, email, pwd, id])).rows;
-  return data;
+  try {
+    await client.query('BEGIN');
+    const sql = `update users set 
+      name =$1, surname=$2, email = $3, pwd = $4
+      where id = $5 returning *`;
+    const data = (await client.query(sql, [name, surname, email, pwd, id])).rows;
+    await client.query('COMMIT');
+    return data;
+  } catch (error) {
+    await client.query('ROLLBACK');
+    console.log(`updateUserDB: ${error.message}`);
+    return [];
+  }
 }
 
 async function patchUsersDB(id, clientObj) {
   const client = await pool.connect();
-  const sql = `select * from users where id = $1`;
-  const oldObj = (await client.query(sql, [id])).rows;
-  const newObj = { ...oldObj[0], ...clientObj };
-  const sqlUpdate = `update users set name = $1, surname = $2, email = $3, pwd = $4
-    where id = $5 returning *`;
-  const result = (await client.query(sqlUpdate, [newObj.name, newObj.surname, newObj.email, newObj.pwd, id])).rows;
-  return result;
+  try {
+    await client.query('BEGIN');
+    const sql = `select * from users where id = $1`;
+    const oldObj = (await client.query(sql, [id])).rows;
+    const newObj = { ...oldObj[0], ...clientObj };
+    const sqlUpdate = `update users set name = $1, surname = $2, email = $3, pwd = $4
+      where id = $5 returning *`;
+    const result = (await client.query(sqlUpdate, [newObj.name, newObj.surname, newObj.email, newObj.pwd, id])).rows;
+    await client.query('COMMIT');
+    return result;
+  } catch (error) {
+    await client.query('ROLLBACK');
+    console.log(`patchUserDB: ${error.message}`);
+    return [];
+  }
 }
 
 async function deleteUserByIdDB(id) {
   const client = await pool.connect();
-  const sql = `delete from users where id =$1 returning *`;
+  try {
+    await client.query('BEGIN');
+    const sql = `delete from users where id =$1 returning *`;
 
-  const data = (await client.query(sql, [id])).rows;
-
-  return data;
+    const data = (await client.query(sql, [id])).rows;
+    await client.query('COMMIT');
+    return data;
+  } catch (error) {
+    await client.query('ROLLBACK');
+    console.log(`deleteUserDB: ${error.message}`);
+    return [];
+  }
 }
 
 module.exports = { getAllUsersDB, getUserIDDB, createUsersDB, updateUsersDB, patchUsersDB, deleteUserByIdDB };
